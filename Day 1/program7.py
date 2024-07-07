5e142591a10965625730ad9845251251a6a585ba
✔️Arbaz Khan <arbazkhaan.cs@gmail.com>
2024-07-07 20:48:30 +0530

    Update program7.py

--- Day 1/program7.py
+++ Day 1/program7.py
@@ -2,3 +2,4 @@ num = int(input("Enter a number: ")) # Get user input
 i = 1
 while i <= 10: # Loop through numbers from 1 to 10
     print(f"{num} x {i} = {num * i}") # Print the multiplication table
+    i+=1
